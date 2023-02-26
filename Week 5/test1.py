68427d409984c724c5e4f364debe646e55f98a8d
Md-Awaf <md.awafattar123@gmail.com>
2023-02-26 17:01:00 +0530

    Second Assignment completed

--- Week 5/test1.py
+++ Week 5/test1.py
@@ -4,8 +4,8 @@ adj_list = []
 def bfs(adj_list, n, u):
     queue = []
     queue.append(u)
-    visited = [False] * (n)
-    path = [0] * (n)
+    visited = [False] * (n + 1)
+    path = [0] * (n + 1)
     visited[u] = True
     while len(queue) > 0:
         u = queue.pop(0)
@@ -19,13 +19,14 @@ def bfs(adj_list, n, u):
 def bfs2(adj_list, n, u):
     queue = []
     queue.append(u)
-    visited = [False] * (n)
-    path = [0] * (n)
+    visited = [False] * (n + 1)
+    path = [0] * (n + 1)
     visited[u] = True
-    while len(queue) > 0:
+    count = 0
+    while len(queue) > 0 and count<20:
         # print(queue)
         u = queue.pop(0)
-        for i in range(n):
+        for i in range(1,n+1):
             if i in adj_list[u]:    
                 # print(i)
                 continue
@@ -36,25 +37,31 @@ def bfs2(adj_list, n, u):
             queue.append(i)
             visited[i] = True
             path[i] = path[u] + 1
+            count += 1
     return path
 
 
-for i in range(n):
+for i in range(n + 1):
     adj_list.append(list())
 for i in range(m):
     u, v = list(map(int,input().split()))
-    adj_list[u-1].append(v-1)
-    adj_list[v-1].append(u-1)
+    adj_list[u].append(v)
+    adj_list[v].append(u)
 # print(adj_list)
 # print(bfs(adj_list,n,1))
-# print(bfs2(adj_list,n,0))
+# print(bfs2(adj_list,n,1))
 
-m1 = (bfs(adj_list,n,0)[n-1])
-m2 = (bfs2(adj_list,n,0)[n-1])
+m1 = (bfs(adj_list,n,1)[n])
+m2 = (bfs2(adj_list,n,1)[n])
+# if m1 == 0 or m2 == 0:
+#     print("-1", end="")
+# else:
+#     print(max(m1, m2), end="")
+# print(m1, m2)
 
-if n-1 in adj_list[0] and m2 != 0:
+if n in adj_list[1] and m2 != 0:
     print(m2)
 elif m1 != 0 and m2 != 0:
     print(m1)
 else:
-    print("-1")
+    print("-1", end="")
