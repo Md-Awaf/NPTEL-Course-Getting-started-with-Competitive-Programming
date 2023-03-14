9930c40a4be244834a8cd25b2039d29dd0b00b6b
Md-Awaf <md.awafattar123@gmail.com>
2023-03-14 22:27:21 +0530

    Solved Week 7 assignment

---
+++ Week 7/A1-DigitalIndia.py
@@ -0,0 +1,61 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    #  Applying Kruskal algorithm
+    def kruskal_algo(self):
+        # print(len(self.graph))
+        result = []
+        sum = 0
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            # print(i)
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        for u, v, weight in result:
+            sum += weight
+        return sum
+
+tcases = int(input())
+
+for _ in range(tcases):
+    n, m = list(map(int,input().split()))
+    g = Graph(n)
+    for i in range(m):
+        u, v, w = list(map(int,input().split()))
+        g.add_edge(u-1, v-1, w)
+    print(g.kruskal_algo())
+
+
